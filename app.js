55d168bc5a1b1125ac5c805bbc766a6e283877f0
Mochalka <bdauren0408@gmail.com>
2024-03-01 13:24:31 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,151 @@
+// codefree/app.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const passwordRegex = /^[a-zA-Z0-9]{5,}$/;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/codefree', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use(session({
+    secret: '0000',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+
+app.get('/editor', (req, res) => {
+    res.sendFile(__dirname + '/templates/editor.html');
+});
+
+app.post('/run_code', (req, res) => {
+    const code = req.body.code;
+    try {
+        eval(code);
+        res.send('Code executed successfully');
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+});
+
+
+app.get('/profile.html', async (req, res) => {
+    try {
+        if (!req.session || !req.session.userId) {
+            return res.redirect('/login');
+        }
+        const userId = req.session.userId;
+        const currentUser = await User.findById(userId);
+        if (!currentUser) {
+            return res.status(404).send('User not found');
+        }
+        res.sendFile(__dirname + '/templates/profile.html'); // Отправляем HTML страницу профиля
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        if (!passwordRegex.test(password)) {
+            return res.send('Password must be at least 5 characters long and contain only letters and digits.');
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.send('Username already exists. Choose a different one.');
+        }
+
+        // Хэшируем пароль перед сохранением
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        return res.send(`
+            <p>Registration successful!</p>
+            <a href="/editor"><button>Press to move next page</button></a>
+        `);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        // Используем метод findOne с фильтрами, чтобы найти пользователя по имени пользователя
+        const user = await User.findOne({ username });
+
+        // Проверяем, найден ли пользователь и сравниваем хэшированный пароль
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.send('Invalid username or password.');
+        }
+
+        req.session.userId = user._id;
+        return res.send(`
+            <p>Login successful!</p>
+            <a href="/editor"><button>Press to move next page</button></a>
+        `);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/templates/index.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/templates/register.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/templates/login.html');
+});
+
+app.get('/editor', (req, res) => {
+    res.sendFile(__dirname + '/templates/editor.html');
+});
+
+app.get('/education', (req, res) => {
+    res.sendFile(__dirname + '/templates/education.html');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/templates/about.html');
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
